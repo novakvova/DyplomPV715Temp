a7aef57b707566f104bf9324c5b1835948981f79
Yana <yana2003kon@gmail.com>
2020-07-23 17:47:28 +0300

    Add class Subjects

---
+++ WebUI/Entities/Subjects.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Entities
+{
+    public class Subjects
+    {
+        //{
+        //    public int Subject_Id { get; set; }
+        //    public string Subject_Name { get; set; }
+        //TODO: public virtual
+
+        /// public Subjects() {   }
+
+        [Key]
+        public int Subject_Id { get; set; }
+        public string Subject_Name { get; set; }
+    }
+}
+
